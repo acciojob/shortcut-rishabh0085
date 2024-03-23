820dfbd1547669f5d7cf9c7c10ef8631c2660ce1
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2024-03-23 18:56:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function shortcut(s1, s2) {
-  // your code here
+  // Check if either of the input strings is empty
+  if (s1 === '' || s2 === '') {
+    return '';
+  }
+  
+  // Extract the initial letters of both strings
+  const initialS1 = s1.charAt(0);
+  const initialS2 = s2.charAt(0);
+  
+  // Concatenate the initial letters and return
+  return initialS1 + initialS2;
 }
 
 // Do not change the code below.
